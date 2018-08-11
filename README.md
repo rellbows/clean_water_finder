e1f352898e1c90dfce44e8be73141aaaaa052a6f
rellbows <ryan.ellis927@gmail.com>
2018-08-11 10:27:03 -0500

    Initial commit with basic report form setup. Form has not been linked up to DB yet, so reports are not saved on the DB currently.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # clean_water_finder
 Application to help users locate/request clean water and broadcast water advisories.
+
+-- Skills --
+node.js, express, mysql 
---
+++ app.js
@@ -0,0 +1,46 @@
+// Program Name: LOTR DB App
+// Filename: lotr_app.js
+// Author: Ryan Ellis
+// Description: Main node.js script for the LOTR database web app
+
+// SERVER SIDE CODE
+
+var express = require('express');
+var mysql = require('./dbcon.js');
+var bodyParser = require('body-parser');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static('public')); // where static .js files are served
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+app.set('mysql', mysql);
+
+//brings up homepage
+app.get('/', function(req, res){
+	res.render('home.handlebars')
+});
+
+//brings up characters page
+app.use('/add_report', require('./add_report.js'));
+
+//bad url route
+app.use(function(req, res){
+	res.status(404);
+	res.render('404');
+});
+
+//bad syntax route
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.type('plain/text');
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+	console.log('Express started on http://localhost:' + app.get('port') + '; Press Ctrl-C to terminate.');
+});
